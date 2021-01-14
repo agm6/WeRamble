a3d2d7ec768b3158ef0e93064000d4c10dd0fc15
fs65 <55656397+fs65@users.noreply.github.com>
2021-01-14 12:47:23 +0000

    Update log.ts

--- components/src/modules/middle/log.ts
+++ components/src/modules/middle/log.ts
@@ -1,1 +1,9 @@
+import { MiddlewareFn } from "type-graphql";
+import { appContext } from "../../types/appContext";
 
+export const logger: MiddlewareFn<appContext> = async ({ args }, next) => {
+
+    console.log("args:", args);
+
+    return next();
+};
