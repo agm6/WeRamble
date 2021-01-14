8a8fc9e14b64b211e966dc6c3cddae9a08e27130
fs65 <55656397+fs65@users.noreply.github.com>
2021-01-14 12:11:15 +0000

    Update createUrl.ts

--- components/src/modules/util/createUrl.ts
+++ components/src/modules/util/createUrl.ts
@@ -1,1 +1,12 @@
+import { v4 } from "uuid";
+import { redis } from "../../redis";
+import { ip, port } from "../util"
+
+export const createUrl = async (userId: number) => {
+
+    const token = v4();
+    await redis.set(token, userId, "ex", 60*60*24); //expires after 1 day
+
+    return `http://${ip}:${port}/api/user/confirm/${token}`;
+};
 
