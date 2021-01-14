9e00cf70737ec778ae3030a827596e3e1ed32293
fs65 <55656397+fs65@users.noreply.github.com>
2021-01-14 12:42:46 +0000

    Update auth.ts

--- components/src/modules/middle/auth.ts
+++ components/src/modules/middle/auth.ts
@@ -1,1 +1,12 @@
+import { MiddlewareFn } from "type-graphql";
+import { appContext } from "../../types/appContext";
 
+export const auth: MiddlewareFn<appContext> = async ({ context }, next) => {
+
+    if(!context.req.session!.userId) {
+
+        throw new Error("you havent confirmed your email yet");
+    }
+
+    return next();
+};
