44857fd60e1f804d6e1a5f81c9825ac7d6ab7c85
fs65 <55656397+fs65@users.noreply.github.com>
2021-01-14 12:24:14 +0000

    Update sendEmail.ts

--- components/src/modules/util/sendEmail.ts
+++ components/src/modules/util/sendEmail.ts
@@ -1,1 +1,28 @@
+import nodemailer from "nodemailer";
 
+export async function sendEmail(email: string, url: string) {
+
+    const account = await nodemailer.createTestAccount();
+
+    const transporter = nodemailer.transporter({
+
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: true, // use SSL
+        auth: {
+         user: 'werambleservices@gmail.com',
+         pass: '0089fxcy?'
+        }   
+    });
+
+    const mailOptions = {
+
+        from: '"Fred Foo 👻" <foo@example.com>', // sender address
+        to: email, // list of receivers
+        subject: "Hello ✔", // Subject line
+        text: "Hello world?", // plain text body
+        html: `<a href="${url}">${url}</a>` // html body
+    }
+
+    const info = await transporter.sendEmail(mailOptions);
+}
