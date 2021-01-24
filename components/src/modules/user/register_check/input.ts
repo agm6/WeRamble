7314daf4fbe94069c175d15cc24059f8bc9b9e46
fs65 <55656397+fs65@users.noreply.github.com>
2021-01-24 13:53:35 +0000

    Update input.ts

--- components/src/modules/user/register_check/input.ts
+++ components/src/modules/user/register_check/input.ts
@@ -1,1 +1,24 @@
+import { Length, IsEmail } from "class-validator";
+import { Field, InputType } from "type-graphql";
+import { email_exist } from "./email_exist";
+
+@InputType()
+export class input {
+
+    @Field()
+    @Length(1, 255) 
+    firstName: string;
+
+    @Field()
+    @Length(1, 255)
+    lastName: string;
+
+    @Field()
+    @IsEmail()
+    @email_exist({message: "that email is already in use"})
+    email: string;
+
+    @Field()
+    password: string;
+}
 
