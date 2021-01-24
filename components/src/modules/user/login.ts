e1e9e61e709b28526f0cb5049df7ed6b3a5b0748
fs65 <55656397+fs65@users.noreply.github.com>
2021-01-24 14:41:45 +0000

    Update login.ts

--- components/src/modules/user/login.ts
+++ components/src/modules/user/login.ts
@@ -1,1 +1,37 @@
+import{Resolver, Mutation, Arg, Ctx} from 'type-graphql';
+import bycrypt from "bcryptjs";
+import {User} from "../../entity/User";
+import{appContext} from "../../types/appContext";
 
+@Resolver()
+export class LoginResolver {
+    @Mutation(() => User, {nullable: true})
+    aysnc login(
+        @Arg("email") email: string,
+        @Arg("password") password: string,
+        @Ctx() ctx: appContext
+    ):  Promise<User | null> {
+
+        const user = await User.findOne({where: {email}});
+
+        if(!user) {
+            return null;
+        }
+
+        const valid = await bycrypt.compare(password, user.password);
+        
+        if(!valid) {
+
+            return null;
+        }
+
+        if(!user.confirmed) {
+
+            return null;
+        }
+
+        ctx.req.session!.userId = user.id;
+
+        return user;
+    }
+}
