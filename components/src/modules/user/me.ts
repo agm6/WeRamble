597b8f5718d75c9eea7d98ea2bf51254a1c17300
fs65 <55656397+fs65@users.noreply.github.com>
2021-01-24 14:49:44 +0000

    Update me.ts

--- components/src/modules/user/me.ts
+++ components/src/modules/user/me.ts
@@ -1,1 +1,19 @@
+import {Resolver, Query, Ctx} from "type-graphql";
+import {User} from "../../entity/User";
+import {appContext} from "../../types/appContext";
+
+@Resolver()
+export class MeResolver {
+
+    @Query(() => User, {nullable: true})
+    async me(@Ctx() ctx: appContext): Promise<User | undefind> {
+
+        if(!ctx.req.session!.userId) {
+
+            return undefined;
+        }
+
+        return User.findOne(ctx.req.session!.userId);
+    }
+}
 
