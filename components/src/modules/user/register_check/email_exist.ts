bff696e87a5331c530fccf3803e5839ca1e7bb89
fs65 <55656397+fs65@users.noreply.github.com>
2021-01-24 14:15:21 +0000

    Update email_exist.ts

--- components/src/modules/user/register_check/email_exist.ts
+++ components/src/modules/user/register_check/email_exist.ts
@@ -1,1 +1,34 @@
+import {registerDecorator} from "class-validator";
+import {ValidationOptions} from "class-validator";
+import {ValidatorConstraint} from "class-validator";
+import {ValidatorConstraintInterface} from "class-validator";
+import {User} from "../../../entity/User";
 
+@ValidatorConstraint({async: true}) 
+export class IsEmailAlreadyExistConstraint
+    implements ValidatorConstraintInterface {
+
+        validate(email: string) {
+
+            return User.findOne({where: {email}}).then(user => {
+
+                if(user) return false;
+                return true;
+            });
+        }
+    }
+
+export function IsEmailAlreadyExist(validationOptions?: ValidationOptions) {
+
+    return function(object: Object, propertyName: string) {
+
+        registerDecorator({
+
+            target: object.constructor,
+            propertyName: propertyName,
+            options: validationOptions,
+            constraints: [],
+            validator: IsEmailAlreadyExistConstraint
+        });
+    };
+}
